495eb4a29588941e4de1a711a1ed979326a72a31
dhruval0 <dhruvaldhorajia2014@gmail.com>
2022-04-19 11:33:27 +0530

    Changed all files and resolved error of module not found

--- drfproj/views.py
+++ drfproj/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from drfapp.serializers import StudentSerializer
-from drfapp.models import student
+from drfapp.models import Student
 class TesView(APIView):
     def get(self, request, *args, **kwargs):
         data = {
